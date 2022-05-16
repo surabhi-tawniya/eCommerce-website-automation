4f93454ede10ce82cc033f746986255a742540ec
SurabhiTawniya <surabhitawniya@gmail.com>
2022-05-17 00:37:34 +0530

    Registration functionality on E-commerece online shopping website

--- src/test/java/OnlineShoppingStepDef.java
+++ src/test/java/OnlineShoppingStepDef.java
@@ -2,13 +2,17 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 public class OnlineShoppingStepDef {
@@ -46,18 +50,21 @@ public class OnlineShoppingStepDef {
 
     @And("user enters email id in create an account section")
     public void userEntersEmailIdInCreateAnAccountSection() {
-        driver.findElement(By.cssSelector("[name='email_create']")).sendKeys("surabhi.s.oza@gmail.com");
+        String randomEmail = RandomStringUtils.random(20, true,true);
+        String email = randomEmail +"@gmail.com";
+        driver.findElement(By.id("email_create")).sendKeys(email);
     }
 
     @And("clicks create an account button")
     public void clicks_create_an_account_button() {
-        driver.findElement(By.xpath("//div[@class='submit']")).click();
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("SubmitCreate"))).click();
     }
 
     @And("user enters his personal information")
     public void user_enters_his_personal_information() {
-        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
-        driver.findElement(By.xpath("//input[@name='id_gender']")).click();
+        driver.manage().timeouts().implicitlyWait(6000, TimeUnit.MILLISECONDS);
+        driver.findElement(By.id("id_gender1")).click();
         driver.findElement(By.name("customer_firstname")).sendKeys("Test User");
         driver.findElement(By.name("customer_lastname")).sendKeys("Vsoft");
         driver.findElement(By.id("passwd")).sendKeys("PKR@PKR");
@@ -77,21 +84,24 @@ public class OnlineShoppingStepDef {
         Select country = new Select(countrydropDown);
         country.selectByVisibleText("United States");
 
-        driver.findElement(By.id("phone_mobile")).sendKeys("234567890");
-        driver.findElement(By.xpath("//input[@name=\"alias\"]")).clear();
+        driver.findElement(By.id("phone_mobile")).sendKeys("9856278980");
+
         driver.findElement(By.xpath("//input[@name=\"alias\"]")).sendKeys("Office");
-        driver.findElement(By.name("submitAccount")).click();
-        String userText = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).getText();
+
     }
 
     @And("clicks on Registration button")
     public void clicks_on_registration_button() {
-        driver.findElement(By.xpath("//*[@id=\"submitAccount\"]/span0")).click();
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"submitAccount\"]/span"))).click();
     }
 
     @Then("user created successfully")
     public void user_created_successfully() {
-        String userText = null;
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.titleIs("My Store"));
+
+        String userText = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).getText();
         if (userText.contains("Vsoft")) {
             System.out.println("User Verified,Test case Passed");
         } else {
--- src/test/resources/features/online_shopping.feature
+++ src/test/resources/features/online_shopping.feature
@@ -3,7 +3,7 @@ Feature: Registration functionality on E-commerece online shopping website
   Scenario: As a user I want to register or sign in
     Given browser is open
     When user navigates to url {string}
-    And  verifying that the home page is visible successfully
+    Then verifying that the home page is visible successfully
     And user clicks on Sign-in link
     And user enters email id in create an account section
     And clicks create an account button
